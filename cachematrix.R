5ad1f061212cea3281bc71a04787be5b1aafcc2f
invfx <simos.kalap@gmail.com>
2015-09-27 21:46:38 +0300

    Code for inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Programming Assignment 2 [R programming] --Cashing  Inverse Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+    
+  }
+  
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
+  
 }
 
-
-## Write a short comment describing this function
+# Calculate the Inverse of "makeCasheMatrix". If the inverse has allready been calculated  
+# it will retrieve that one.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)                     ## Return a matrix that is the inverse of 'x'
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
+  
 }
